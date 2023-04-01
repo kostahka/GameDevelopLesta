05212c1e91cd3db0a6da6ecdb4b7421823698a7d
Maksim <maks_kosten@mail.ru>
2023-04-01 18:25:06 +0300

    Fix dll export/import.

--- 01-2-hellolib/lib/include/hello-lib.hxx
+++ 01-2-hellolib/lib/include/hello-lib.hxx
@@ -1,3 +1,19 @@
-#include <string_view>
+#ifndef HELLO_LIB
+
+#define HELLO_LIB
+
+#ifdef WIN32
+
+#ifdef PROJECTLIBRARY_EXPORTS
+#define LIB_API __delspec(dllexport)
+#else
+#define LIB_API __delspec(dllimport)
+#endif
+
+#else
+#define LIB_API
+#endif
 
 bool hello_world();
+
+#endif
