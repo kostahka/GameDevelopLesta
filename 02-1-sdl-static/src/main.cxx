f8194d47bc690059c87c6c68e5c011cfd6e85f7a
Maksim <maks_kosten@mail.ru>
2023-04-08 16:19:34 +0300

    Customize color of version sdl.

--- 02-1-sdl-static/src/main.cxx
+++ 02-1-sdl-static/src/main.cxx
@@ -3,19 +3,32 @@
 #include <iostream>
 #include <ostream>
 
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+
+#define BHBLU "\e[1;94m"
+#define BHMAG "\e[1;95m"
+
+#define COLOR_RESET "\e[0m"
+
 std::ostream& operator<<(std::ostream& output, const SDL_version& version)
 {
-    return output << static_cast<int>(version.major) << "."
-                  << static_cast<int>(version.minor) << "."
-                  << static_cast<int>(version.patch);
+    return output << GRN << static_cast<int>(version.major) << "." << YEL
+                  << static_cast<int>(version.minor) << "." << RED
+                  << static_cast<int>(version.patch) << COLOR_RESET;
 }
 
 int main()
 {
+    SDL_version compiled{ 0, 0, 0 };
     SDL_version linked{ 0, 0, 0 };
+
+    SDL_VERSION(&compiled);
     SDL_GetVersion(&linked);
 
-    std::cout << linked << std::endl;
+    std::cout << BHBLU << "Compiled version:" << compiled << std::endl;
+    std::cout << BHMAG << "Linked version:" << linked << std::endl;
 
     return std::cout.good() ? EXIT_SUCCESS : EXIT_FAILURE;
 }
