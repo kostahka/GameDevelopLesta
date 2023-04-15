a15c361b8a2888bc73ad95bcd11f8d678bd46e0f
Maksim <maks_kosten@mail.ru>
2023-04-15 23:09:10 +0300

    Fix includes.

--- 03-1-sdl-loop/src/main.cxx
+++ 03-1-sdl-loop/src/main.cxx
@@ -1,8 +1,4 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_error.h>
-#include <SDL3/SDL_events.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_video.h>
 #include <algorithm>
 #include <array>
 #include <cstdlib>
